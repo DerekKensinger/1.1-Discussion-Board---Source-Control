e4870f542c2b52fb02ebea25cc235467dcd81d58
Derek Kensinger <dkensinger40@gmail.com>
2022-06-02 23:14:52 -0400

    Add project files.

---
+++ 1.1 Discussion Board - Source Control/1.1 Discussion Board - Source Control.cpp
@@ -0,0 +1,32 @@
+// 1.1 Discussion Board - Source Control.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+     
+    std::cout << "1.1 Discussion Board - Source Control \n"
+        "·      https ://github.com/BenBrostoff/draftfast.git\n"
+        "o   Something that I have loved since I have gotten older is sports betting.I am by no means addicted, I just have always loved sportsand having skin in the game makes watching more fun.I have used optimizers you need to pay for but it was cool to look under the hoodand what all went in to building that.\n"
+        "·      https ://github.com/ushahidi/Ushahidi_Web.git\n"
+        "o   I work here at Full Sail Universityand I am apart of the Outreach Team.We use an app that we pay for to efficiently collect information from perspective students.Being able to create a form app that can be used in both Wifiand locally would be so huge for our department.\n"
+        "·      https ://github.com/addtek/reactnative_sports_betting_app.git\n"
+        "o   As with the first project I talked about my love for sports betting.I found this project super interesting because it was fun to see all that goes into creating a sports book from a programming standpoint.Seeing all that went into it is so complex but fit into the scope of what I was expecting.\n"
+        "·      https ://github.com/brettv30/Call-of-Duty-Game-Predictions.git\n"
+        "o   I enjoy watching the CDLand having stats that can be able to help predict a map outcome would be very valuable.Especially when I love to sports bet, that prediction could be very helpful."
+        "·      https ://github.com/github/gitignore.git\n"
+        "o   These will be helpful not only in this class but moving forward.I am adding this to my personal repo.";
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
